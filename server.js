341d4d4d8415452ccdcb30afc8c68ed90c383ca2
Kwaku Dapaah <kwa2the4@gmail.com>
2018-09-02 01:17:47 -0400

    finished with all routes for project and actions

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const actionModel = require('./data/helpers/actionModel');
 const mappers = require('./data/helpers/mappers');
 const projectModels =require('./data//helpers/projectModel');
 const server = express();
+const cors = require('cors');
 
 // 
 server.use(morgan('combined'));
@@ -13,7 +14,7 @@ server.use(helmet());
 server.use(express.json());
 
 
-// routes
+// routes(projects)
 server.get('/', (req, res) => {
     res.send("Welcome to the Project");
     console.log("welcome to the Project");
@@ -59,7 +60,68 @@ server.post('/projects', async (req, res) => {
     }
 })
 
+server.put('/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    const project = req.body;
+    try {
+        const response = await projectModels.update(id, project);
+        res.status(201).json(response);
+    } catch(ex) {
+        res.status(500).json({ message: "Cannot update Project" });
+    }
+})
 
+server.delete('/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response =  await projectModels.remove(id);
+        res.status(201).json(response);
+    } catch(err) {
+        res.status(500).json({ message: "Cannot delete project"});
+    }
+})
+
+// routes(actions)
+
+server.get('/actions', async (req, res) => {
+    try {
+        const response = await actionModel.get();
+        res.status(200).json(response);
+    } catch(err) {
+        res.status(500).json({ message: "cannot retrieve actions"})
+    }
+})
+
+server.post('/actions', async (req, res) => {
+    const action = req.body;
+    try {
+        const response = await actionModel.insert(action);
+        res.status(201).json(response);
+    } catch(err) {
+        res.status(500).json({ message: "Cannot create Action"});
+    }
+})
+
+server.put('/actions/:id', async (req, res) => {
+    const { id } = req.params;
+    const newAction = req.body;
+    try {
+        const response = await actionModel.update(id, newAction);
+        res.status(200).json(response);
+    } catch(err) {
+        res.status(500).json({ message: "cannot update action"});
+    }
+ })
+
+ server.delete('/actions/:id', async (req, res) => {
+     const { id } = req.params;
+     try {
+         const response = await actionModel.remove(id);
+         res.status(200).json(response);
+     } catch(err) {
+         res.status(500).json({ message: "cannot remove action"});
+     }
+ })
 
 //
 const PORT = 8000;
